2a9abd9f143211a0ddade996bdbc880eec04d4e4
fricklerhandwerk <valentin@fricklerhandwerk.de>
2019-02-18 00:27:58 +0100

    use home-manager as system package and wrap per user
    this creates a base state for user configuration on system rebuild, and
    copies the configuration used into user's `$HOME` if there is none on
    each call to `home-manager`.
    
    unfortunately the user still has to call `home-manager` at least once to
    get `.config` into its home directory, but this may be omitted if
    copying is not done in a wrapper but a user service instead.
    
    since the user details are private, this part is not visible on the
    public branch.

--- configuration.nix
+++ configuration.nix
@@ -1,53 +1,22 @@
 { config, pkgs, ... }:
-  let home-manager = pkgs.home-manager.overrideAttrs ( oldAttrs:{
-    src = pkgs.fetchFromGitHub {
-      owner = "fricklerhandwerk";
-      repo = "home-manager";
-      rev = "643dff586d3e9d676571f712688563a0e5f14f97";
-      sha256 = "0jxfw1hgfiiir308pw83wsgs33wijhyj0dwm8bxviqlyrkwwzs97";
-    };
-    buildCommand = ''
-      install -v -D -m755 ${home-manager.src}/home-manager/home-manager $out/bin/home-manager
-      substituteInPlace $out/bin/home-manager \
-        --subst-var-by bash "${pkgs.bash}" \
-        --subst-var-by coreutils "${pkgs.coreutils}" \
-        --subst-var-by findutils "${pkgs.findutils}" \
-        --subst-var-by gnused "${pkgs.gnused}" \
-        --subst-var-by less "${pkgs.less}" \
-        --subst-var-by nix "${pkgs.nix}" \
-        --subst-var-by HOME_MANAGER_PATH '${home-manager.src}'
-     '';
-   }); in
+with builtins;
+with pkgs;
 {
   imports =
     [
       ./hardware-configuration.nix
+      "${hmsrc}/nixos"
     ];
-    boot.loader.systemd-boot.enable = true;
-    boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
 
-    networking.wireless.enable = true;
+  networking.wireless.enable = true;
 
-    environment.systemPackages = with pkgs; [
-      neovim
-      home-manager
-    ];
-
-    # TODO: run this only for specific user
-    system.userActivationScripts = {
-      home = {
-        text = ''
-          # assuming absence of `~/.config` is equivalent to a fresh install,
-          # fetch user config from remote repository
-          if [[ ! -d ~/.config ]]; then
-            ${pkgs.git}/bin/git clone git://github.com/fricklerhandwerk/.config.git
-            ${home-manager}/bin/home-manager switch
-          fi
-        '';
-        deps = [];
-
-      };
-    };
+  environment.systemPackages = with pkgs; [
+    neovim
+    git
+    home-manager
+  ];
 
   system.stateVersion = "18.09";
 }
