21e5b5adc974f1626b1d4a689a4d724de59c3965
fricklerhandwerk <valentin@fricklerhandwerk.de>
2019-03-17 15:24:30 +0100

    add custom systemd service for user config
    this is essentially following the main nixos manual advice to use systemd user
    services for all post-rebuild setup, and it works.
    
    a more sophisticated approach would be to `git clone` to a tempdir and
    ask the repository where it should live within $HOME, and where
    `home-manager` config should be taken from within the repository. but
    I feel this would be too much configuration instead of relying convention. having
    everything in default locations is not just easier to handle, but also
    allows skipping `git clone` altogether if `.config` is already populated.

--- configuration.nix
+++ configuration.nix
@@ -5,12 +5,56 @@ let
   home-manager = import (
     fetchTarball https://github.com/rycee/home-manager/archive/release-18.09.tar.gz
     ) { inherit pkgs; };
+  home-config = { config, lib, pkgs, utils, ... }:
+    with lib;
+    let
+      users = config.home-config.users;
+    in
+    {
+      options = {
+        home-config.users = mkOption {
+          description = "per-user home config repository";
+          type = types.attrsOf (types.submodule {
+            options = {
+              repo = mkOption {
+                type = types.str;
+                description = "git repository with user configuration";
+              };
+            };
+          });
+        };
+      };
+      config = mkIf (users != {}) {
+        systemd.services = mapAttrs' (username: cfg:
+          nameValuePair ("home-config-${utils.escapeSystemdPath username}") {
+            description = "initial home-manager configuration for ${username}";
+            wantedBy = [ "multi-user.target" ];
+            wants = [ "nix-daemon.socket" ];
+            after = [ "nix-daemon.socket" ];
+            serviceConfig = {
+              User = username;
+              Type = "oneshot";
+              SyslogIdentifier = "home-config-${username}";
+              ExecStart = pkgs.writeScript "home-config-${username}"
+              ''
+                #! ${pkgs.stdenv.shell} -el
+                if [[ ! -d $HOME/.config ]]; then
+                  cd $HOME
+                  ${pkgs.git}/bin/git clone ${cfg.repo}
+                  ${home-manager.home-manager}/bin/home-manager switch
+                fi
+              '';
+            };
+          }
+        ) users;
+      };
+    };
 in
 {
   imports =
     [
       ./hardware-configuration.nix
-      home-manager.nixos
+      home-config
     ];
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
