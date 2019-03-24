a70133f42517e701569ee3461dbf839caf806c1c
fricklerhandwerk <valentin@fricklerhandwerk.de>
2019-03-24 22:23:50 +0100

    use uxterm with Ubunutu Mono

--- nixpkgs/home.nix
+++ nixpkgs/home.nix
@@ -5,6 +5,15 @@
   ];
   xsession = {
     enable = true;
-    windowManager.xmonad.enable = true;
+    windowManager.xmonad = {
+      enable = true;
+      config = pkgs.writeText "xmonad.hs" ''
+        import XMonad
+        main = xmonad defaultConfig {
+          terminal = "uxterm -fa 'Ubuntu Mono'"
+        , borderWidth = 3
+        }
+      '';
+    };
   };
 }
