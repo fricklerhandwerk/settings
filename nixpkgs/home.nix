9e486f872f701ed787613a9d60f88cc288fabe5a
fricklerhandwerk <valentin@fricklerhandwerk.de>
2019-03-25 02:24:39 +0100

    add dmenu to xmonad

--- nixpkgs/home.nix
+++ nixpkgs/home.nix
@@ -1,18 +1,23 @@
 { pkgs, ... }:
 {
   home.packages = with pkgs; [
+    dmenu
     qutebrowser
   ];
   xsession = {
     enable = true;
     windowManager.xmonad = {
       enable = true;
+      enableContribAndExtras = true;
       config = pkgs.writeText "xmonad.hs" ''
         import XMonad
-        main = xmonad defaultConfig {
-          terminal = "uxterm -fa 'Ubuntu Mono'"
-        , borderWidth = 3
-        }
+        import XMonad.Util.EZConfig(additionalKeys)
+        main = xmonad $ defaultConfig
+          { terminal = "uxterm -fa 'Ubuntu Mono'"
+          , borderWidth = 3
+          } `additionalKeys`
+          [ ((mod1Mask, xK_p), spawn "dmenu_run")
+          ]
       '';
     };
   };
