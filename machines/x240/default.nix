0bb8a2200b501f1431a3015d7ef09addc1094e06
fricklerhandwerk <valentin@fricklerhandwerk.de>
2019-04-19 21:01:49 +0200

    use networkmanager
    tired of messing with wpa_cli manually, as it confuses the hell out of
    me. the command line semantics of nmcli are not much better, but at
    least they are sort of discoverable.

--- machines/x240/default.nix
+++ machines/x240/default.nix
@@ -11,7 +11,7 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.wireless.enable = true;
+  networking.networkmanager.enable = true;
 
   services.xserver = {
     enable = true;
