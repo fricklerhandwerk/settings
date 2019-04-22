6b81f7f81db4a79b666bb32bb15a6e9c43115f0f
fricklerhandwerk <valentin@fricklerhandwerk.de>
2019-04-22 21:55:16 +0200

    use default xmobar to have at least some status bar
    this is just a stopgap before I figure out how to properly set up a
    status bar and which exactly. who would have thought this is such a
    complex endeavor?

--- nixpkgs/xmonad/config.hs
+++ nixpkgs/xmonad/config.hs
@@ -1,16 +1,23 @@
 import XMonad
 import XMonad.Util.EZConfig(additionalKeys)
-main = xmonad $ defaultConfig
-  { modMask = mod4Mask
-  -- pass config as command line parameters for now to prevent creating more
-  -- dotfiles. eventually there should be a setup to create an XResources file
-  -- in a custom location.
-  , terminal = "uxterm -ls \
-               \ -fa 'Ubuntu Mono' \
-               \ -xrm '*selectToClipboard: true' \
-               \ -xrm '*metaSendsEscape: true'"
-  , borderWidth = 3
-  , focusFollowsMouse = False
-  } `additionalKeys`
-  [ ((mod4Mask, xK_Return), spawn "$(yeganesh -x)")
-  ]
+import XMonad.Util.Run(spawnPipe)
+import XMonad.Hooks.DynamicLog
+import XMonad.Layout.NoBorders
+
+main = do
+  cfg <- xmobar $ defaultConfig
+    { modMask = mod4Mask
+    -- pass config as command line parameters for now to prevent creating more
+    -- dotfiles. eventually there should be a setup to create an XResources file
+    -- in a custom location.
+    , terminal = "uxterm -ls \
+                 \ -fa 'Ubuntu Mono' \
+                 \ -xrm '*selectToClipboard: true' \
+                 \ -xrm '*metaSendsEscape: true'"
+    , borderWidth = 3
+    , focusFollowsMouse = False
+    , layoutHook = smartBorders $ layoutHook defaultConfig
+    } `additionalKeys`
+    [ ((mod4Mask, xK_Return), spawn "$(yeganesh -x)")
+    ]
+  xmonad cfg
