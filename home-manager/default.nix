e690dcf16294c054d7523211cfaae97dcd44183d
fricklerhandwerk <valentin@fricklerhandwerk.de>
2019-07-08 00:24:34 +0200

    use `stable` channel for packages
    add `tor` browser bundle. it is intended to track the tip of the branch,
    as `tor` is updated frequently and old binaries disappear quickly.

--- home-manager/default.nix
+++ home-manager/default.nix
@@ -1,5 +1,14 @@
 { pkgs, ... }:
 with builtins;
+let
+  unstable = let
+    nixpkgs-unstable = fetchGit {
+      name = "nixpkgs-unstable";
+      url = "https://github.com/NixOS/nixpkgs-channels";
+      ref = "nixpkgs-unstable";
+    };
+    in import nixpkgs-unstable { config = pkgs.config; };
+in
 {
   home.packages = with pkgs; [
     git
@@ -8,6 +17,7 @@ with builtins;
     qutebrowser
     ranger
     vlc
+    unstable.tor-browser-bundle-bin
   ];
   programs.fish.enable = true;
   home.sessionVariables = {
