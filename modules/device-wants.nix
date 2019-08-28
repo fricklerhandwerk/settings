7dbab8842170cd1ea4b971c80e9b26ffe8c7939e
fricklerhandwerk <valentin@fricklerhandwerk.de>
2019-08-28 15:27:58 +0200

    fix typo in description

--- modules/device-wants.nix
+++ modules/device-wants.nix
@@ -3,16 +3,16 @@
   config = {
     systemd.user.services.udev-system-user-wants = {
       unitConfig = {
-        Description = "udev ENV{SYSTEM_USER_WANTS} units";
+        Description = "udev ENV{SYSTEMD_USER_WANTS} units";
         Documentation = "file://${pkgs.writeText "doc" ''
-          Start all ENV{SYSTEM_USER_WANTS} units from udev.
+          Start all ENV{SYSTEMD_USER_WANTS} units from udev.
           This is a workaround to let udev rules properly trigger user services.
         ''}";
       };
       wantedBy = [ "default.target" ];
       serviceConfig = {
         Type = "oneshot";
-        ExecStart = with pkgs; writeScript "get-system_user_wants" ''
+        ExecStart = with pkgs; writeScript "get-systemd_user_wants" ''
           #! ${stdenv.shell} -el
           PATH=${buildPackages.udev}/bin:$PATH
           PATH=${coreutils}/bin:$PATH
