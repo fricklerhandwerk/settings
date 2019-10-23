33f6958a2ec61a44469d29e5658679581f25ffc8
fricklerhandwerk <valentin@fricklerhandwerk.de>
2019-10-23 10:42:15 +0200

    pin wine version

--- user/modules/wine.nix
+++ user/modules/wine.nix
@@ -1,11 +1,19 @@
 { pkgs, ... }:
-with pkgs;
-# latest wine available
-let wine = (unstable.wine.override { wineRelease = "unstable"; });
+let
+  src = fetchGit {
+    name = "nixpkgs-unstable";
+    url = "https://github.com/NixOS/nixpkgs-channels";
+    ref = "nixpkgs-unstable";
+    # pin wine to some known-working release, using impure `unstable` will often
+    # provoke lengthy recompilation
+    rev = "f35f0880f2cdbc8c1bc81492811251f120d7a9bc";
+  };
+  unstable = pkgs.callPackage src { config = pkgs.config; };
+  wine = unstable.wine.override { wineRelease = "unstable"; };
+  winetricks = unstable.winetricks.override { wine = wine; };
 in
 {
   home.packages = [
     wine
-    (unstable.winetricks.override { wine = wine; })
   ];
 }
