f4bd3c261c5b96fc252a0d6413eab71197986de6
fricklerhandwerk <valentin@fricklerhandwerk.de>
2019-10-31 13:21:08 +0100

    use new nvim configuration options

--- user/modules/nvim/default.nix
+++ user/modules/nvim/default.nix
@@ -4,32 +4,32 @@
     enable = true;
     viAlias = true;
     vimAlias = true;
-    configure = {
-      customRC = "${builtins.readFile ./init.vim}";
-      packages.plugins = with pkgs.vimPlugins;
-        let unstable = pkgs.unstable.vimPlugins; in {
-        start = [
-          unstable.vim-fish
-          fugitive
-          vim-nix
-          vim-surround
-          vim-repeat
-          unstable.lexima-vim
-          ctrlp-vim
-          vim-abolish
-          vim-better-whitespace
-          nerdcommenter
-          ultisnips
-          vim-snippets
-          deoplete-nvim
-          echodoc
-          vim-airline
-          vim-airline-themes
-          unstable.NeoSolarized
-          LanguageClient-neovim
-        ];
-      };
-    };
+    extraConfig = "${builtins.readFile ./init.vim}";
+    plugins =
+      with pkgs.vimPlugins;
+      let
+        unstable = pkgs.unstable.vimPlugins;
+      in
+      [
+        unstable.vim-fish
+        fugitive
+        vim-nix
+        vim-surround
+        vim-repeat
+        unstable.lexima-vim
+        ctrlp-vim
+        vim-abolish
+        vim-better-whitespace
+        nerdcommenter
+        ultisnips
+        vim-snippets
+        deoplete-nvim
+        echodoc
+        vim-airline
+        vim-airline-themes
+        unstable.NeoSolarized
+        LanguageClient-neovim
+      ];
   };
   home.packages = with pkgs; [
     (python3.withPackages (ps: [
