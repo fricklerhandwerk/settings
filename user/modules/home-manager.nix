6e73a00bb6ecc757d166b48c0c57fc21c1df5635
fricklerhandwerk <valentin@fricklerhandwerk.de>
2019-10-31 13:20:41 +0100

    bump home-manager release

--- user/modules/home-manager.nix
+++ user/modules/home-manager.nix
@@ -1,10 +1,11 @@
 { pkgs ? import <nixpkgs> {} }:
 with pkgs;
 let
+  version = "19.09";
   src = builtins.fetchGit {
-    name = "home-manager-19.03";
+    name = "home-manager-${version}";
     url = https://github.com/rycee/home-manager;
-    ref = "release-19.03";
+    ref = "release-${version}";
   };
 in
 callPackage "${src}/home-manager" { path = "${src}"; }
