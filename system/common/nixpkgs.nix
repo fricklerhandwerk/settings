7f56d9c334c62dcd4d9f3cecbbbb5c08d2776e0c
fricklerhandwerk <valentin@fricklerhandwerk.de>
2019-12-02 15:08:18 +0100

    update nixpkgs to 19.09

--- system/common/nixpkgs.nix
+++ system/common/nixpkgs.nix
@@ -1,6 +1,6 @@
 fetchGit {
   name = "nixpkgs-channels";
   url = "https://github.com/NixOS/nixpkgs-channels";
-  ref = "nixos-19.03";
-  rev = "520c39049f585c05f216bc12c088e4f5fd988d73";
+  ref = "nixos-19.09";
+  rev = "dae3575cee5b88de966d06b11861c602975cb23a";
 }
