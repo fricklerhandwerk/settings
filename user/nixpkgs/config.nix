0d60f474a041b775092b63d9cbe245336f1393ab
fricklerhandwerk <valentin@fricklerhandwerk.de>
2020-12-28 20:10:43 +0100

    add nix config with working sandbox builds
    this seems to be the only current workaround that allows sandbox builds
    on Darwin.
    
    https://github.com/NixOS/nix/issues/2732#issuecomment-474150868

---
+++ user/nixpkgs/config.nix
@@ -0,0 +1,5 @@
+{
+  allowUnfree = true;
+  sandbox = true;
+  extra-sandbox-paths = [ /System/Library/Frameworks /System/Library/PrivateFrameworks /usr/lib /private/tmp /private/var/tmp /usr/bin/env ];
+}
