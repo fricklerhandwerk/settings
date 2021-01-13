e08ed9974318e276e2850c8c12105ea2fe81e4ea
fricklerhandwerk <valentin@fricklerhandwerk.de>
2021-01-13 01:00:38 +0100

    wine: upgrade to 5.16
    this also makes use of the cache again

--- user/modules/linux/graphical/wine.nix
+++ user/modules/linux/graphical/wine.nix
@@ -1,20 +1,14 @@
 { pkgs, ... }:
 let
-  src = fetchGit {
-    name = "nixpkgs-unstable";
-    url = "https://github.com/NixOS/nixpkgs-channels";
-    ref = "nixpkgs-unstable";
-    # pin wine to some known-working release, using impure `unstable` will often
-    # provoke lengthy recompilation
-    rev = "f35f0880f2cdbc8c1bc81492811251f120d7a9bc";
+  src = fetchTarball {
+    # NixOS 20.09 2021-01-13
+    url = "https://github.com/NixOS/nixpkgs/archive/aa5b9cd16b91d8a24681c3888951d7c3dc612314.tar.gz";
+    sha256 = "1cyz0xhsy8wa5bf1mh9cind3xvlf5wqz3k4dr9kdhd08wd978f6f";
   };
-  unstable = pkgs.callPackage src { config = pkgs.config; };
-  wine = unstable.wine.override { wineRelease = "unstable"; };
-  winetricks = unstable.winetricks.override { wine = wine; };
+  nixos-20-09 = pkgs.callPackage src { config = pkgs.config; };
+  wine = nixos-20-09.wineWowPackages.staging;
+  winetricks = nixos-20-09.winetricks.override { inherit wine; };
 in
 {
-  home.packages = [
-    wine
-    winetricks
-  ];
+  home.packages = [ wine winetricks ];
 }
