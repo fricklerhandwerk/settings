c74390d6f72f24b459c9848e317cddcf3698d941
fricklerhandwerk <valentin@fricklerhandwerk.de>
2021-02-10 16:51:29 +0100

    gpg-agent: fallback to curses pinentry in console

--- user/modules/common/git.nix
+++ user/modules/common/git.nix
@@ -6,6 +6,23 @@ in
 {
   programs.git = {
     enable = true;
+    package = with pkgs; symlinkJoin {
+      name = "git";
+      paths = [ gitMinimal ];
+      buildInputs = [ makeWrapper ];
+      # make `git-remote-gcrypt` and therefore `gpg` aware of the calling
+      # `tty`.  this is needed because `git` runs its remote-helpers without
+      # `tty` hooked up. while this is not a problem in an `xsession` where
+      # `$DISPLAY` is set and we get a graphical prompt, `gpg` fails to pick up
+      # the password from `pinentry-curses` in an actual console without that
+      # variable set correctly.
+      # this took a whole day to find out and finally fix.
+      # NOTE: this should not be an overlay, otherwise all kinds of stuff that
+      # depends on `git` (such as `wine`) will have to be rebuilt.
+      postBuild = ''
+        wrapProgram $out/bin/git --run "export GPG_TTY=\$(tty)"
+      '';
+    };
     extraConfig = {
       core = {
         pager = "${diff} | ${less}";
