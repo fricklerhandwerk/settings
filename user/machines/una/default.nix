1a0d53dc919b32f631cabec1005be985e89fc065
fricklerhandwerk <valentin@fricklerhandwerk.de>
2021-06-08 17:25:55 +0200

    add service to update SMBC comics archive
    this is a user service, because the `git` credentials are user-specific.
    this allows for a fairly simple update service.

--- user/machines/una/default.nix
+++ user/machines/una/default.nix
@@ -2,6 +2,7 @@
 {
   imports = [
     ../../profiles/linux
+    ./smbc.nix
   ];
 
   machine = ./.;
---
+++ user/machines/una/smbc.nix
@@ -0,0 +1,41 @@
+{ pkgs, lib, ... }:
+{
+  # daily systemd serivce to scrape comics and push updates to master
+  systemd.user.services.smbc-update =
+  let
+    smbc = builtins.fetchGit {
+      url = "ssh://git@github.com/fricklerhandwerk/smbc";
+      ref = "master";
+    };
+    pyEnv = (import "${smbc}").pyEnv;
+    push-update = pkgs.writeShellScriptBin "push-update" ''
+      export PATH=${with pkgs; lib.makeBinPath [ pyEnv git openssh coreutils ]}:$PATH
+      set -e
+      today=$(date --rfc-3339 date)
+      git checkout master
+      git pull origin master
+      script/scrape.py
+      git add source/comics
+      git commit -m "automatic update"
+      git push origin master
+    '';
+  in {
+    Unit = { Description =  "update SMBC comics archive"; };
+    Service = {
+      Environment = [
+        "GIT_SSH_COMMAND='ssh -i $HOME/.ssh/github -o IdentitiesOnly=yes'"
+      ];
+      # assumes this repository is already present here:
+      WorkingDirectory = "%h/smbc";
+      ExecStart = "${push-update}/bin/push-update";
+    };
+  };
+  systemd.user.timers.smbc-update = {
+    Unit = { Description = "daily update of SMBC comics archive"; };
+    Timer = {
+      Unit = "smbc-update.service";
+      OnCalendar = "daily";
+    };
+    Install = { WantedBy = [ "timers.target" ]; };
+  };
+}
