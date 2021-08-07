47a26692c38a857b19674655dcfdbf1774c998a9
fricklerhandwerk <valentin@fricklerhandwerk.de>
2021-08-07 17:21:15 +0200

    use plasma desktop environment

--- system/common/x11.nix
+++ system/common/x11.nix
@@ -2,14 +2,8 @@
 {
   services.xserver = {
     enable = true;
-    displayManager.lightdm.enable = true;
-    displayManager.defaultSession = "none+fake";
-    displayManager.session =
-      let fakeSession = {
-        manage = "window";
-        name = "fake";
-        start = "";
-      }; in [ fakeSession ];
+    displayManager.sddm.enable = true;
+    desktopManager.plasma5.enable = true;
     xkbOptions = "altwin:swap_lalt_lwin";
   };
   console.useXkbConfig = true;
