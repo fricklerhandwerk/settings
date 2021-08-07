7d5ebee0a00cb06ea417b814c0f8232f57739e2b
fricklerhandwerk <valentin@fricklerhandwerk.de>
2021-08-07 19:19:29 +0200

    simplify audio setup on `x240`
    all that is handled by `plasma5` now

--- system/machines/x240/default.nix
+++ system/machines/x240/default.nix
@@ -5,12 +5,11 @@
     ../../common/local.nix
     ../../common/x11.nix
     ./boot.nix
-    ./audio.nix
-    ./backlight.nix
     ./hardware.nix
     ./thinkpad.nix
     ./trackpad.nix
   ];
 
   services.logind.lidSwitch = "hibernate";
+  hardware.pulseaudio.enable = true;
 }
